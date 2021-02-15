2631e0d2268020ee1bdf703d0363bb6fdd9102a2
Ariel <arielthibault@yahoo.fr>
2021-02-16 00:36:48 +0100

    Auth made
    Middleware added and making of the feature for:
    -register
    -login
    -get user data
    -logout

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth','namespace'=>'Auth'], function () {
+    
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/me', [AuthController::class, 'me']);
+    });
 });
+
